48a36137a07023ac34c22a817f254e3cb4390971
Qin-shihuang <0.0@owo.li>
2024-04-17 14:58:33 +0800

    Doc: Added docs for Allocator

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ use buddy::Heap;
 
 pub struct Allocator<const ORDER: usize>(Mutex<Heap<ORDER>>);
 
+/// A generic allocator implementation.
 impl<const ORDER: usize> Allocator<ORDER> {
     pub const fn new() -> Self {
         Allocator(Mutex::new(Heap::new()))
@@ -28,7 +29,10 @@ impl<const ORDER: usize> Deref for Allocator<ORDER> {
     }
 }
 
+/// Implements the `GlobalAlloc` trait for the `Allocator` struct.
 unsafe impl<const ORDER: usize> GlobalAlloc for Allocator<ORDER> {
+    /// Allocates a block of memory with the given layout. 
+    /// If successful, returns a pointer to the block of memory, else returns a null pointer.
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         self.0
             .lock()
@@ -37,6 +41,7 @@ unsafe impl<const ORDER: usize> GlobalAlloc for Allocator<ORDER> {
             .map_or(core::ptr::null_mut(), |ptr| ptr.as_ptr())
     }
 
+    /// Deallocates the block of memory pointed to by `ptr` with the given layout.
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         self.0
             .lock()
